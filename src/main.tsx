15450ff841aa5272152c2d7c56be7b864f6bf812
vygandas <vygandas.pliasas@gmail.com>
2023-08-31 15:01:12 +0300

    small fixes with ts and svg

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+reportWebVitals();
