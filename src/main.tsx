a169fd0552db8cdadb62e7ede03bd192f03064ad
vygandas <vygandas.pliasas@gmail.com>
2023-08-31 15:15:18 +0300

    installed libraries according to task requirements

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,38 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+
+const Style = createGlobalStyle`
+  * { box-sizing: border-box; }
+  body{ margin:0; }
+`;
+
+const theme = {
+  fontSizes: [12, 14, 16, 24, 32, 48, 64, 96, 128],
+  space: [
+    // margin and padding
+    0, 4, 8, 16, 32, 64, 128, 256,
+  ],
+  colors: {
+    blue: "#07c",
+    red: "#e10",
+  },
+};
+
+const Root = styled.div`
+  font-family: system-ui, sans-serif;
+  line-height: 1.5;
+`;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Root>
+        <Style />
+        <App />
+      </Root>
+    </ThemeProvider>
   </React.StrictMode>,
 );
 
