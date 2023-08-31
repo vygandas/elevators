d4f01708ecea16b51628712f717ca8b9a7cd9119
Vygandas Pliasas <vygandas.pliasas@gmail.com>
2023-08-31 17:07:36 +0300

    remove coverage folder

--- src/main.tsx
+++ src/main.tsx
@@ -2,37 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import reportWebVitals from "./reportWebVitals";
-import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 const Style = createGlobalStyle`
   * { box-sizing: border-box; }
-  body{ margin:0; }
+  body{ font-family: system-ui, sans-serif; line-height: 1.5; }
 `;
 
 const theme = {
   fontSizes: [12, 14, 16, 24, 32, 48, 64, 96, 128],
-  space: [
-    // margin and padding
-    0, 4, 8, 16, 32, 64, 128, 256,
-  ],
+  space: [0, 4, 8, 16, 32, 64, 128, 256],
   colors: {
     blue: "#07c",
     red: "#e10",
   },
 };
 
-const Root = styled.div`
-  font-family: system-ui, sans-serif;
-  line-height: 1.5;
-`;
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Root>
+      <>
         <Style />
         <App />
-      </Root>
+      </>
     </ThemeProvider>
   </React.StrictMode>,
 );
